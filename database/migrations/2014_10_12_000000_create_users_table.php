03799c1794e640d2f90a401d0a4e574ceccf8fcc
Raziye Moradi <raziye.moradi73@gmail.com>
2023-02-15 12:58:22 +0330

    add confirmation_code column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone_number')->unique();
             $table->integer('debt');
+            $table->integer('confirmation_code')->unique()->nullable();
             $table->timestamps();
         });
     }
