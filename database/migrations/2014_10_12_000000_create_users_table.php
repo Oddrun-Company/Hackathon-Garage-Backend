90370be87c9f5795ec76439ad58f4d986614a8ee
fatemes802 <fatemes802@gmail.com>
2023-02-16 02:11:55 +0330

    fix: remove confirmation code from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone_number')->unique();
             $table->integer('debt');
-            $table->integer('confirmation_code')->unique()->nullable();
             $table->timestamps();
         });
     }
